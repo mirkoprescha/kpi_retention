b05304023fe65d42d165b8fc3c380018173c4254
mprescha <mirko.prescha@immobilienscout24.de>
2017-04-03 09:52:49 +0200

    add command line parser

---
+++ src/main/scala/spaxploder/ArrayExploder.scala
@@ -0,0 +1,41 @@
+package spaxploder
+
+import net.sourceforge.argparse4j.ArgumentParsers
+import net.sourceforge.argparse4j.inf.{ArgumentParser, ArgumentParserException, Namespace}
+
+
+object ArrayExploder {
+
+  def main(args: Array[String]) {
+
+    val parsedArgs: Namespace = parseArgs(args)
+    val inputPath = parsedArgs.getString("input.path")
+    val inputFileformat = parsedArgs.getString("input.fileformat")
+    val arrayName = parsedArgs.getString("array.name")
+    val primaryKey = parsedArgs.getString("primary.key")
+    val outputPath = parsedArgs.getString("output.path")
+    val outputFileformat = parsedArgs.getString("output.fileformat")
+
+
+  }
+
+  def parseArgs(args: Array[String]): Namespace = {
+    val parser: ArgumentParser = ArgumentParsers.newArgumentParser("spaxploder").description("SPark Array eXPLODER - convert array values into rows").defaultHelp(true)
+    parser.addArgument("--input.path").required(true).help("path to input files containing array")
+    parser.addArgument("--input.fileformat").choices("parquet","json").help("fileformat of input files")
+    parser.addArgument("--array.name").required(true).help("field name of the array that values will be converted into rows")
+    parser.addArgument("--primary.key").required(true).help("field name of the id to reference entity of parent row")
+    parser.addArgument("--output.path").required(true).help("path to output of exploded array values")
+    parser.addArgument("--output.fileformat").choices("parquet","json").help("fileformat of output")
+     try {
+      parser.parseArgs(args)
+    } catch  {
+      case ioe: ArgumentParserException => {
+        parser.handleError(ioe)
+        System.exit(1)
+        val ns:Namespace = null
+        ns
+      }
+     }
+  }
+}
---
+++ src/main/scala/spaxploder/InputArray.scala
@@ -0,0 +1,16 @@
+package spaxploder
+
+import org.apache.spark.sql.Dataset
+
+class InputArray {
+  def readArrayIntoDataset(
+  inputPath : String,
+   inputFileformat: String,
+   arrayName : String,
+    primaryKey : String
+
+  ): Dataset {
+
+  }
+
+}
