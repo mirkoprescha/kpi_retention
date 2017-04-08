c47e2e480e55e22982645deac06ee0b39ed2fe80
mirko <mirko>
2017-04-08 10:00:07 +0200

    schemaBuilder creates idArraySchema

--- src/main/scala/com/mirkoprescha/spaxploder/SchemaBuilder.scala
+++ src/main/scala/com/mirkoprescha/spaxploder/SchemaBuilder.scala
@@ -1,17 +1,28 @@
 package com.mirkoprescha.spaxploder
 
-import org.apache.spark.sql.types.{DataType, StructType}
+import org.apache.spark.sql.types._
 
 
 class SchemaBuilder {
 
-  def schema(
-                              arrayName: String,
+  val nonDecimalTypes =  Seq(NullType,ByteType,ShortType ,IntegerType, LongType, FloatType, DoubleType,StringType,BinaryType,BooleanType, DateType,TimestampType)
+  val complexTypes = Seq ( ArrayType, MapType, StructType /*, StructField*/)
+
+  private val nonDecimalNameToType = {
+    nonDecimalTypes.map(t => t.typeName -> t).toMap
+  }
+
+
+  /*
+    creates schema consisting of id and an array containing simple DataType
+   */
+  def idArraySchema(
                               primaryKey: String,
-                              arrayElementType: String,
-                              primaryKeyType: String
+                              primaryKeyTypeName: String,
+                              arrayName: String,
+                              arrayElementTypeName: String
                             ) : StructType = {
-    ???
+    new StructType().add(primaryKey,primaryKeyTypeName).add(arrayName,DataTypeFromName("array",arrayElementTypeName))
   }
 
   /*
@@ -19,6 +30,18 @@ class SchemaBuilder {
      eg. "integer" -> "IntegerType"
    */
   def DataTypeFromName (name: String) : DataType ={
-    ???
+    val FIXED_DECIMAL = """decimal\(\s*(\d+)\s*,\s*(\-?\d+)\s*\)""".r
+    name match {
+      case "decimal" => DecimalType.USER_DEFAULT
+      case FIXED_DECIMAL(precision, scale) => DecimalType(precision.toInt, scale.toInt)
+      case other => nonDecimalNameToType(other)
+    }
+  }
+  def DataTypeFromName (name: String, elementType: String) : DataType ={
+    name match {
+      case "array" => DataTypes.createArrayType(DataTypeFromName(elementType))
+      case "map" => ??? // DataTypes.createMapType(DataTypeFromName(elementType))
+      case "struct" => ??? // DataTypes.createStructType(DataTypeFromName(elementType))
+    }
   }
 }
--- src/test/scala/com/mirkoprescha/spaxploder/SchemaBuilderSpec.scala
+++ src/test/scala/com/mirkoprescha/spaxploder/SchemaBuilderSpec.scala
@@ -1,80 +1,48 @@
 package com.mirkoprescha.spaxploder
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.Row
 import org.apache.spark.sql.types._
 import org.scalatest.{MustMatchers, FlatSpec}
 
-/**
-  * Created by mirko on 06.04.17.
-  */
 class SchemaBuilderSpec extends FlatSpec with MustMatchers {
-
-
+  val datatypes = Seq(NullType,ByteType,ShortType ,IntegerType, LongType, FloatType, DoubleType,StringType,BinaryType,BooleanType, DateType,TimestampType)
+  val dataTypeNames = datatypes.map(t => t -> t.typeName)
 
 
   behavior of "DataTypeFromName"
 
   it must "create all simple non-decimal datatypes" in {
-
-  }
-
-  it must "create  decimal datatypes" in {
-
-  }
-
-
-  it must "create  an ArrayType with elements of StringType" in {
-
-  }
-
-  it must "create  an ArrayType with elements of LongType" in {
-
-  }
-
-  val nonDecimalTypes =  Seq(NullType,ByteType,ShortType ,IntegerType, LongType, FloatType, DoubleType,StringType,BinaryType,BooleanType, DataType)
-  val complexTypes = Seq ( ArrayType, MapType, StructType /*, StructField*/)
-
-  val arrayOfStringSchema = new StructType().add("id",StringType).add("myArray",ArrayType(StringType),nullable = true)
-  val arrayOfIntSchema = new StructType().add("id",StringType).add("myArray",ArrayType(IntegerType),nullable = true)
-  val arrayOfStructSchema = new StructType().add("id",StringType).add("myArray",ArrayType(StructType(Seq(StructField("id",StringType)))),nullable = true)
-
-
-  private val nonDecimalNameToType = {
-    Seq(NullType, DateType, TimestampType, BinaryType, IntegerType, BooleanType, LongType,
-      DoubleType, FloatType, ShortType, ByteType, StringType, CalendarIntervalType,TimestampType)
-      .map(t => t.typeName -> t).toMap
-  }
-
-  Seq(LongType,StringType).map(t => t.typeName)
-  val s: DataType =   ArrayType(StringType)
-
-  /** Given the string representation of a type, return its DataType */
-  def nameToType(name: String): DataType = {
-    val FIXED_DECIMAL = """decimal\(\s*(\d+)\s*,\s*(\-?\d+)\s*\)""".r
-    name match {
-          case "decimal" => DecimalType.USER_DEFAULT
-          case FIXED_DECIMAL(precision, scale) => DecimalType(precision.toInt, scale.toInt)
-          case other => nonDecimalNameToType(other)
+    dataTypeNames.foreach  {dt =>
+      println("Testing " + dt.toString())
+      dt._1.getClass must be (new SchemaBuilder().DataTypeFromName(dt._2).getClass)
     }
   }
 
+  // "decimal" without precision or scale not tested
+  it must "create decimal datatypes" in {
+    DecimalType(10, 5).toString() must be(new SchemaBuilder().DataTypeFromName("decimal(10,5)").toString)
+  }
+
+  it must "create an ArrayType with elements of StringType" in {
+    val underTest: DataType = new SchemaBuilder().DataTypeFromName("array", "string")
+    underTest.asInstanceOf[ArrayType].elementType must be (StringType)
+  }
+
+  it must "create an ArrayType with elements of LongType" in {
+    val underTest: DataType = new SchemaBuilder().DataTypeFromName("array", "long")
+    underTest.asInstanceOf[ArrayType].elementType must be (LongType)
+  }
+
+
+
+  behavior of "SchemaBuilder.schema"
   val sb = new SchemaBuilder()
-  "Array with String-Elements" must "...." in {
-    nonDecimalNameToType.foreach(println(_))
 
-    val c= DataTypes.createArrayType(nameToType("long"))
-    c.typeName
-
-    val b: DataType = nameToType("string")
-    println(" StringType should be " + b.toString)
-
-    val stringSchema = new StructType().add("id",b).add("myArray",c)
-    println(stringSchema.simpleString)
-    //    types.foreach(x => println(x.toString))
-//    types.foreach(x => println(x.catalogString))
-      val underTest = sb.schema("myArray","id","StringType", "StringType")
-    underTest must be   equals(arrayOfStringSchema)
+  it must "create schema with an array of String-Elements" in {
+    val underTest: StructType = sb.idArraySchema(primaryKey = "id", primaryKeyTypeName = "long", arrayName =  "myArray",arrayElementTypeName =  "string")
+    val expected: StructType = new StructType().add("id","long").add("myArray",DataTypes.createArrayType(StringType))
+    println(expected)
+    println(underTest)
+    underTest must be  (expected)
   }
 
 }
